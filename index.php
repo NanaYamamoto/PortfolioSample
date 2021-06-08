82aa131a1ab308f2e3e091bad50a9a344a954dee
nana <chyhyy76@users.noreply.github.com>
2021-06-08 13:42:17 +0900

    index.html,style.css追加

--- index.php
+++ index.php
@@ -13,9 +13,68 @@
     <header>
         <h1  class="header-title">
             <a href="">
-            <span>P</span class="color-blue">ortfolio
+            <span class="color-blue">P</span>ortfolio
             </a>
         </h1>
     </header>
+
+    <div class="box-main">
+        <div class="box-profile">
+            <h2>
+                <span class="color-blue">P</span>rofile
+            </h2>
+            <div class="profile">
+                <img src="img/nana.JPG" alt="プロフィール画像">
+                <b>Nana Yamamoto</b>
+                <p class="color-blue">HTML / CSS / PHP / MySQL / Laravel / Adobe XD</p>
+                <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+            </div>
+            <div class="icons">
+                <img src="img/github.jpg" alt="githubのアイコン" >
+                <img src="img/twitter.jpg" alt="Twitterのアイコン">
+            </div>
+        </div>
+
+        <div class="box-works">
+            <h2>
+                <span class="color-blue">W</span>orks
+            </h2>
+
+            <div class="works">
+                <div class="work">
+                    <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
+                    <h3>Twitterクローン</h3>
+                    <p>HTML / CSS / PHP / MySQL</p>
+                    <a href="">Webサイト</a>
+                    <a href="">Github</a>
+                </div>
+           
+                <div class="work">
+                    <div class="work">
+                        <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
+                        <h3>カリキュラム管理システム</h3>
+                        <p>Codeigniter / MySQL</p>
+                        <a href="">Webサイト</a>
+                        <a href="">Github</a>
+                    </div>
+                </div>
+                <div class="work">
+                    <div class="work">
+                        <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
+                        <h3>ラーニングシステム</h3>
+                        <p>Laravel / MySQL</p>
+                        <a href="">Webサイト</a>
+                        <a href="">Github</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    
+    <footer>
+        <p>Copyright © TECH I.S., Inc.</p>
+    </footer>
 </body>
 </html>
