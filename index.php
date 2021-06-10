1cd2eb32a06b5e644293bd7b2d8041da896551ed
nana <chyhyy76@users.noreply.github.com>
2021-06-10 12:09:22 +0900

    レスポンシブ対応

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--レスポンシブの設定-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" ontent="自分のポートフォリオ">
     <title>PORTFOLIO</title>
@@ -20,54 +21,57 @@
 
     <div class="box-main">
         <div class="box-profile">
-            <h2>
+            <h2 class="sub-title">
                 <span class="color-blue">P</span>rofile
             </h2>
-            <div class="profile">
+            <div class="item-list">
                 <img src="img/nana.JPG" alt="プロフィール画像">
                 <b>Nana Yamamoto</b>
                 <p class="color-blue">HTML / CSS / PHP / MySQL / Laravel / Adobe XD</p>
                 <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
             </div>
-            <div class="icons">
+            <div class="icon-list">
                 <img src="img/github.jpg" alt="githubのアイコン" >
                 <img src="img/twitter.jpg" alt="Twitterのアイコン">
             </div>
         </div>
 
         <div class="box-works">
-            <h2>
+            <h2 class="sub-title">
                 <span class="color-blue">W</span>orks
             </h2>
 
-            <div class="works">
-                <div class="work">
+            <ul class="works-list">
+                <li>
                     <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
                     <h3>Twitterクローン</h3>
                     <p>HTML / CSS / PHP / MySQL</p>
-                    <a href="">Webサイト</a>
-                    <a href="">Github</a>
-                </div>
-           
-                <div class="work">
-                    <div class="work">
-                        <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
-                        <h3>カリキュラム管理システム</h3>
-                        <p>Codeigniter / MySQL</p>
+                    <div class="link">
                         <a href="">Webサイト</a>
                         <a href="">Github</a>
                     </div>
-                </div>
-                <div class="work">
-                    <div class="work">
-                        <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
-                        <h3>ラーニングシステム</h3>
-                        <p>Laravel / MySQL</p>
+                </li>
+
+                <li>
+                    <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
+                    <h3>カリキュラム管理システム</h3>
+                    <p>Codeigniter / MySQL</p>
+                    <div class="link">
                         <a href="">Webサイト</a>
                         <a href="">Github</a>
                     </div>
-                </div>
-            </div>
+                </li>
+
+                <li>
+                    <img src="img/work_img.jpg" alt="tech.isのTwitter画像">
+                    <h3>ラーニングシステム</h3>
+                    <p>Laravel / MySQL</p>
+                    <div class="link">
+                        <a href="">Webサイト</a>
+                        <a href="">Github</a>
+                    </div>
+                </li>
+            </ul>
 
         </div>
 
